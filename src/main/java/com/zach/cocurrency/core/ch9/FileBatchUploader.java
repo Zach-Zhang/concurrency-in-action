2c5bb13f69de726990592a02cd0ff70551882842
Zach-Zhang <zach_ing@163.com>
2021-11-08 07:51:03 +0800

    第五章线层间的协作

--- src/main/java/com/zach/cocurrency/core/ch9/FileBatchUploader.java
+++ src/main/java/com/zach/cocurrency/core/ch9/FileBatchUploader.java
@@ -2,7 +2,10 @@ package com.zach.cocurrency.core.ch9;
 
 import com.zach.cocurrency.utils.Debug;
 import com.zach.cocurrency.utils.Tools;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPReply;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -24,17 +27,17 @@ public class FileBatchUploader implements Closeable {
     private final String ftpServer;
     private final String userName;
     private final String password;
-    private final String targetRemoterDir;
+    private final String targetRemoteDir;
     private final FTPClient ftp = new FTPClient();
     private final CompletionService<File> completionService;
     private final ExecutorService worker;
     private final ExecutorService dispatcher;
 
-    public FileBatchUploader(String ftpServer, String userName, String password, String targetRemoterDir) {
+    public FileBatchUploader(String ftpServer, String userName, String password, String targetRemoteDir) {
         this.ftpServer = ftpServer;
         this.userName = userName;
         this.password = password;
-        this.targetRemoterDir = targetRemoterDir;
+        this.targetRemoteDir = targetRemoteDir;
         this.worker = Executors.newSingleThreadExecutor();
         this.dispatcher = Executors.newSingleThreadExecutor();
         this.completionService = new ExecutorCompletionService<>(worker);
@@ -50,7 +53,7 @@ public class FileBatchUploader implements Closeable {
         });
     }
 
-    private void doUploadFiles(Set<File> files) {
+    private void doUploadFiles(Set<File> files) throws InterruptedException {
         //批量提交文件长传任务
         for (final File file : files) {
             completionService.submit(new UploadTask(file));
@@ -72,6 +75,20 @@ public class FileBatchUploader implements Closeable {
             completionService.submit(new UploadTask(file));
         }
         //检查md5上传的结果
+        int successUploaded = md5Files.size();
+        for (int i = 0; i < successUploaded; i++) {
+            Future<File> future = completionService.take();
+            try {
+                File uploadFile = future.get();
+                md5Files.remove(uploadFile);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        //将剩余(即未上成功)md5的文件移动到相应的备份目录中
+        for (File file : md5Files) {
+            moveToDeadDir(file);
+        }
     }
 
     private File generateMD5(File file) throws IOException, NoSuchAlgorithmException {
@@ -115,9 +132,52 @@ public class FileBatchUploader implements Closeable {
 
     @Override
     public void close() throws IOException {
-
+        dispatcher.shutdown();
+        try {
+            dispatcher.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        worker.shutdown();
+        try {
+            worker.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Tools.silentClose(() -> {
+            if (ftp.isConnected()) {
+                ftp.disconnect();
+            }
+        });
     }
-
+    // 初始化FTP客户端
+    public void init() throws Exception {
+        FTPClientConfig config = new FTPClientConfig();
+        ftp.configure(config);
+        int reply;
+        ftp.connect(ftpServer);
+        Debug.info("FTP Reply:%s", ftp.getReplyString());
+        reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new Exception("FTP server refused connection.");
+        }
+        boolean isOK = ftp.login(userName, password);
+        if (isOK) {
+            Debug.info("FTP Reply:%s", ftp.getReplyString());
+        } else {
+            throw new Exception("Failed to login." + ftp.getReplyString());
+        }
+        reply = ftp.cwd(targetRemoteDir);
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new Exception("Failed to change working directory.reply:"
+                    + reply);
+        } else {
+            Debug.info("FTP Reply:%s", ftp.getReplyString());
+        }
+        ftp.setFileType(FTP.ASCII_FILE_TYPE);
+    }
     private class UploadTask implements Callable<File> {
         private final File file;
 
